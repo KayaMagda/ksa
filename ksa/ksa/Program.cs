d8a91a3f7efee9504afd0e0663da61348a73ba8a
Kaya Kopp <k.kopp@neogeo.com>
2023-05-12 14:44:14 +0200

    ablaufstruktur

--- ksa/ksa/Program.cs
+++ ksa/ksa/Program.cs
@@ -1,15 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ksa
+namespace ksa;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        string command = Console.ReadLine();
+        switch (command)
         {
+            case "-n":
+                Console.WriteLine("Annika Schäfer, Marika Lübbers, Kaya Kopp");
+                break;
+            case "-csvimp":
+                CsvImport();
+                break;
+            case "-xmlimp":
+                XmlImport();
+                break;
+            case "-jsonimp":
+                JsonImport();
+                break;
+            default:
+                Console.WriteLine("Unbekannter Befehl. Bekannte Befehle: -n, -csvimp, -xmlimp, -jsonimp");
+                break;
+        }
+        Console.WriteLine("Befehl wurde ausgeführt, drücken Sie irgendeine Taste zum Beenden");
+        Console.ReadKey();
+    }
+
+    static void CsvImport()
+    {
+        List<string> filenames = new List<string>();
+        foreach (string filename in filenames)
+        {
+            Console.WriteLine(filename + " erfolgreich importiert!");
         }
     }
+    static void XmlImport()
+    {
+        List<string> filenames = new List<string>();
+        foreach (string filename in filenames)
+        {
+            Console.WriteLine(filename + " erfolgreich importiert!");
+        }
+    }
+    static void JsonImport()
+    {
+        List<string> filenames = new List<string>();
+        foreach (string filename in filenames)
+        {
+            Console.WriteLine(filename + " erfolgreich importiert!");
+        }
+    }
+    
 }
